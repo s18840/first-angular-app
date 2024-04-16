3bfd66644da2af38246daa95bafe431bded48a09
msroczynsk003 <mikolaj.sroczynski@pwc.com>
2024-04-16 14:52:53 +0200

    added get request for products

---
+++ src/app/catalog/product.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IProduct } from './product.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>('/api/products');
+  }
+}
